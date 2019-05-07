f934314976ad1bc92691756c5fd32f2009d77811
kkksssyyy <kyoheisato0410@gmail.com>
2019-05-07 15:15:44 +0900

    Add user microposts 3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,12 @@
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: 'AKIAUVGISDWGK7L3RHVJ',
-    aws_secret_access_key: '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',
-    region: 'ap-northeast-1'
-  }
-
-  config.fog_directory  = 'railssample1picture'
-  config.cache_storage = :fog
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      # Amazon S3用の設定
+      :provider              => 'AWS',
+      :region                => ENV['S3_REGION'],
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
+    }
+    config.fog_directory = ENV['S3_BUCKET']
+  end
 end
