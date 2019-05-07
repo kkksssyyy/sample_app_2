9d14c2e1f0e61d3c70e2529b3939ce82cf6efbc2
kkksssyyy <kyoheisato0410@gmail.com>
2019-05-07 19:49:35 +0900

    Update

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -12,13 +12,12 @@ class PictureUploader < CarrierWave::Uploader::Base
 #  end
   # storage :fog
   #storage :file
-  if Rails.env.development?
-    storage :file
-  elsif Rails.env.test?
-    storage :file
-  else
+  if Rails.env.production?
     storage :fog
+  else
+    storage :file
   end
+
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
